8471b41ebcd1141da22b1f6f97b12b21d87851ff
Erico Andrei <erico@simplesconsultoria.com.br>
2008-08-02 23:11:02 +0000

    Fixes actions/configure.zcml in order to GenericSetup import/export to work
    svn path=/collective.contentrules.mail/trunk/; revision=69402

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1'
+version = = open(os.path.join("collective", "contentrules", "mail", "version.txt")).read().strip()
 
 setup(name='collective.contentrules.mail',
       version=version,
