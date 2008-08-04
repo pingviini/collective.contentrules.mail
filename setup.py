6d8ede1e72ff0218c89ae7e3fa5edbf64c14696c
Erico Andrei <erico@simplesconsultoria.com.br>
2008-08-04 17:20:28 +0000

    Fixes a typo in setup.py
    svn path=/collective.contentrules.mail/trunk/; revision=69447

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = = open(os.path.join("collective", "contentrules", "mail", "version.txt")).read().strip()
+version = open(os.path.join("collective", "contentrules", "mail", "version.txt")).read().strip()
 
 setup(name='collective.contentrules.mail',
       version=version,
