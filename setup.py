f0bc7938ea1c1c864fb172d78c64a82c89ecf35e
Maurits van Rees <maurits@vanrees.org>
2012-10-18 17:11:37 +0200

    pep8 on setup.py

--- setup.py
+++ setup.py
@@ -5,20 +5,22 @@ version = '0.7.dev0'
 setup(name='collective.contentrules.mail',
       version=version,
       description="Flexible mail content rule",
-      long_description=open("README.txt").read() + "\n" +
-                       open("CHANGES.rst").read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      long_description=(open("README.txt").read() + "\n" +
+                        open("CHANGES.rst").read()),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Framework :: Plone",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          ],
       keywords='',
       author='Ingeniweb',
       author_email='support@ingeniweb.com',
       url='https://github.com/collective/collective.contentrules.mail',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective','collective.contentrules'],
+      namespace_packages=['collective', 'collective.contentrules'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
